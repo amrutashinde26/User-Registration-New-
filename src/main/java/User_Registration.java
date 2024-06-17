955a0e06eb7c71aedb9f019fafb194e821a9e041
Amruta Shinde <amrutashinde2610@gmail.com>
2024-06-17 16:25:35 +0530

    UC2 ADDED

--- src/main/java/User_Registration.java
+++ src/main/java/User_Registration.java
@@ -5,9 +5,12 @@ import java.util.regex.Pattern;
 public class User_Registration {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Name");
+        /*System.out.println("Enter First Name");
         String firstname = sc.next();
-        First_Name(firstname);
+        First_Name(firstname);*/
+        System.out.println("Enter Last Name");
+        String lastname = sc.next();
+        Last_Name(lastname); 
     }
 
     public static String First_Name(String first) {
@@ -22,6 +25,22 @@ public class User_Registration {
         }
         return " ";
 
+    }
+
+    public static String Last_Name(String last)
+    {
+        Pattern pattern = Pattern.compile("[A-z][a-z]{3,}");
+        Matcher matcher = pattern.matcher(last);
+        boolean ismatch = matcher.matches();
+        if(ismatch)
+        {
+            System.out.println("Valid Input");
+        }
+        else
+        {
+            System.out.println("Invalid Input");
+        }
+        return  " ";
     }
 
 }
