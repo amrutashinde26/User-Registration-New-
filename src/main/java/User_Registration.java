f2c7ac5f5fd225abc52f286e5be08643178cc780
Amruta Shinde <amrutashinde2610@gmail.com>
2024-06-17 17:46:50 +0530

    uc3 added

--- src/main/java/User_Registration.java
+++ src/main/java/User_Registration.java
@@ -8,9 +8,12 @@ public class User_Registration {
         /*System.out.println("Enter First Name");
         String firstname = sc.next();
         First_Name(firstname);*/
-        System.out.println("Enter Last Name");
+       /* System.out.println("Enter Last Name");
         String lastname = sc.next();
-        Last_Name(lastname); 
+        Last_Name(lastname); */
+        System.out.println("Enter Email Id");
+        String emailid = sc.next();
+        mail(emailid);
     }
 
     public static String First_Name(String first) {
@@ -24,7 +27,6 @@ public class User_Registration {
             System.out.println("Invalid Input");
         }
         return " ";
-
     }
 
     public static String Last_Name(String last)
@@ -43,4 +45,20 @@ public class User_Registration {
         return  " ";
     }
 
+    public  static  String mail( String mail)
+    {
+        Pattern pattern = Pattern.compile("^([a-zA-z0-9._%-]+@[a-zA-z0-9.-]+\\.[a-zA-Z]{2,})$");
+        Matcher matcher = pattern.matcher(mail);
+        boolean ismatch = matcher.matches();
+        if(ismatch)
+        {
+            System.out.println("Valid Email Id");
+        }
+        else {
+            System.out.println("Invalid Email Id");
+        }
+        return " " ;
+    }
+
+    
 }
